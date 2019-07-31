c8cf84cedb1a39b8b50397671f7144f63bb10304
Rachel Koenig <rmdickinson10@gmail.com>
2019-07-31 16:42:02 -0700

    Submitting clean repo

--- Flask_work/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[])
-    email = StringField('Email')
-    submit = SubmitField('Go Now')
